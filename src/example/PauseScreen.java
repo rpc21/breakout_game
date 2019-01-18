1bf3bb2cab2ea12d417888c872e35715077a7fff
Ryan Culhane <rpc21@duke.edu>
2019-01-18 18:34:29 -0500

    updated PauseScreen to include CheatKeys

--- src/example/PauseScreen.java
+++ src/example/PauseScreen.java
@@ -5,6 +5,15 @@ import javafx.stage.Stage;
 
 public class PauseScreen extends GenericScreen {
 
+    private final String PAUSE_SCREEN_TEXTS = "'L' - add another life\n" +
+            "'R' - reset the level\n" +
+            "'T' - give infinite time\n" +
+            "'D' - Destroy any block by clicking on it\n" +
+            "# keys - Jump to level of that number\n" +
+            "'P' - gain access to all power ups\n"+
+            "BACKSPACE always will return to the start menu";
+
+
     public PauseScreen(StageManager stageManager){
         super(stageManager);
         myScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
